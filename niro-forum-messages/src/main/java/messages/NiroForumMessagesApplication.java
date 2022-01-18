d4fdaf2177e47f60c579fa11e01b76e6cf3f3ce6
Niroshan Devendran <niroshan.devendran.80@gmail.com>
2022-01-18 12:54:11 -0800

    Initial Commit

---
+++ niro-forum-messages/src/main/java/messages/NiroForumMessagesApplication.java
@@ -0,0 +1,13 @@
+package messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiroForumMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiroForumMessagesApplication.class, args);
+	}
+
+}
---
+++ niro-forum-messages/src/test/java/messages/NiroForumMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiroForumMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
